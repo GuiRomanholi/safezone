048d5cb90db1f2c05fcc55b2afb080c5bcb3608b
GuiRomanholi <guilhermeroma343@gmail.com>
2025-05-28 11:54:32 -0300

    comecar Swagger

--- src/main/java/br/com/fiap/safezone/SafezoneApplication.java
+++ src/main/java/br/com/fiap/safezone/SafezoneApplication.java
@@ -1,11 +1,17 @@
 package br.com.fiap.safezone;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+
+//http://localhost:8080/swagger-ui/index.html
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition(info =
+@Info(title = "API SafeZone", description = "API RESTful com Swagger para SafeZone", version = "v1"))
 public class SafezoneApplication {
 
 	public static void main(String[] args) {
